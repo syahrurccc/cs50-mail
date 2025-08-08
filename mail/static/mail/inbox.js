c10f7351fb68d543fe1845363d3d44c94a95ebe5
syahrurccc <syahrur.ramadhan94@gmail.com>
2025-08-08 17:27:49 +0800

    working on email view

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,33 +1,87 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
-  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
+	// Use buttons to toggle between views
+	document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+	document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+	document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+	document.querySelector('#compose').addEventListener('click', compose_email);
+	document.querySelector('#emails-view').addEventListener('click', function(event) {
+		const entry = event.target.closest('.email-lists');
+		if (entry) {
+			show_email(entry)
+		}
+	})
 
-  // By default, load the inbox
-  load_mailbox('inbox');
+	// By default, load the inbox
+	load_mailbox('inbox');
 });
 
 function compose_email() {
 
-  // Show compose view and hide other views
-  document.querySelector('#emails-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'block';
+	// Show compose view and hide other views
+	document.querySelector('#emails-view').style.display = 'none';
+	document.querySelector('#compose-view').style.display = 'block';
 
-  // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+	// Clear out composition fields
+	document.querySelector('#compose-recipients').value = '';
+	document.querySelector('#compose-subject').value = '';
+	document.querySelector('#compose-body').value = '';
+
+	document.querySelector('#compose-submit').addEventListener('click', () => {
+		const recipients = document.querySelector('#compose-recipients').value;
+		const subject = document.querySelector('#compose-subject').value;
+		const body = document.querySelector('#compose-body').value;
+
+		fetch('/emails', {
+		method: 'POST',
+		body: JSON.stringify({
+			recipients: recipients,
+			subject: subject,
+			body: body
+		})
+		})
+		.then(response => response.json())
+		.then(result => {
+			console.log(result);
+		});
+	});
 }
+	
 
 function load_mailbox(mailbox) {
   
-  // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
-  document.querySelector('#compose-view').style.display = 'none';
+	// Show the mailbox and hide other views
+	document.querySelector('#emails-view').style.display = 'block';
+	document.querySelector('#compose-view').style.display = 'none';
 
-  // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+	// Show the mailbox name
+	document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+	fetch(`emails/${mailbox}`)
+	.then(response => response.json())
+	.then(emails => {
+		emails.forEach(email => {	
+			const div = document.createElement('div');
+			div.setAttribute("id", email.id);
+			div.setAttribute("class", "email-lists");
+			div.innerHTML = `
+				<span class="email-sender">${email.sender}</span>
+				<span class="email-subject">${email.subject}</span>
+				<span class="email-timestamp">${email.timestamp}</span>`;
+			document.querySelector('#emails-view').append(div);
+		});
+	});
 }
+
+function show_email(entry) {
+
+	document.querySelector('#emails-view').innerHTML = '';
+
+	fetch(`emails/${entry.id}`)
+	.then(response => response.json())
+	.then(email => {
+		console.log(email)
+		document.querySelector('#emails-view').innerHTML = `<h3>${email.subject}</h3>`;
+	})
+}
+	
